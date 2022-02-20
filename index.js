412d59453b0b1e1699e238a7b4feea0871149b95
Mickey Pearce <mickey.pearce@mail.com>
2022-02-20 21:28:30 +0100

    add date range

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const firstline = require('firstline');
 
 const fileInputName = process.argv[2] || 'input\\myInputFile.csv';
 const fileOutputName = process.argv[3] || 'output\\myOutputFile.csv';
+const dateFrom = Date.parse(process.argv[4]) || Date.parse('1980-01-01 00:00:00')
+const dateTo = Date.parse(process.argv[5]) || Date.parse('2050-01-01 00:00:00')
 
 // Remove (, ), /, . and empty char
 const sanitize = (str) => str.replace(/[\(\)\/. ]/g, '')
@@ -15,7 +17,7 @@ async function extract() {
     const json = csvToJson.fieldDelimiter(',').formatValueByType().getJsonFromCsv(fileInputName);
     console.log(`Reading from file ${fileInputName}.`)
 
-// Clean property names as they cannot contain strange chars
+    // Clean property names as they cannot contain strange chars
     const jsonSane = json.map(o => {
         let newo = {}
         _.forEach(o, (value, key) =>
@@ -25,33 +27,44 @@ async function extract() {
     })
     //  console.log(jsonSane)
 
-// Extract different shares tickers that were sold
+    // Extract different shares tickers that were sold
     const tickerArray = _.uniq(
         _.filter(jsonSane, { Action: 'Market sell' }).map(o => o.Ticker)
     )
     // console.log(tickerArray)
 
-// Get only trades by tickers in tickerArray
+    // Get only trades by tickers in tickerArray
     const filteredJson = jsonSane.filter(o => tickerArray.includes(o.Ticker))
     // console.log(filteredJson)
 
-// Create a map of tickers with arrays of open transaction IDs (buys that don't have sell afterwards)
-    let openTxsMap = {}
+    // Create a map of tickers with arrays of trx IDs to be removed:
+    // - open transactions (buys that don't have sell afterwards)
+    // - closed transactions not in range
+    let toRemoveTxsMap = {}
     for (const tx of filteredJson) {
-        if (!openTxsMap[tx.Ticker]) openTxsMap[tx.Ticker] = []
-        if(tx.Action == 'Market buy') openTxsMap[tx.Ticker].push(tx.ID)
-        if(tx.Action == 'Market sell') openTxsMap[tx.Ticker] = []
+        if (!toRemoveTxsMap[tx.Ticker]) toRemoveTxsMap[tx.Ticker] = []
+        if (tx.Action == 'Market buy') {
+            toRemoveTxsMap[tx.Ticker].push(tx.ID)
+        }
+        // Clear array if sell and in range
+        if ((tx.Action == 'Market sell')) {
+            if ((Date.parse(tx.Time) >= dateFrom) && (Date.parse(tx.Time) <= dateTo)) {
+                toRemoveTxsMap[tx.Ticker] = []
+            } else {
+                toRemoveTxsMap[tx.Ticker].push(tx.ID)
+            }
+        }
     }
-    // console.log(openTxsMap)
+    // console.log(toRemoveTxsMap)
 
     // const openTxs = _.flatten(Object.values(openTxsMap))
-    const openTxs = Object.values(openTxsMap).flat()
+    const toRemoveTxs = Object.values(toRemoveTxsMap).flat()
     // console.log(openTxs)
 
-// Filter out open transaction
-    const finalJson = filteredJson.filter(o => !openTxs.includes(o.ID))
+    // Filter out transaction to be removed
+    const finalJson = filteredJson.filter(o => !toRemoveTxs.includes(o.ID))
 
-// Write json to csv file
+    // Write json to csv file
     const originalHeader = await firstline(fileInputName)
     // console.log(originalHeader)
     const headerMap = originalHeader
---
+++ test.js
@@ -0,0 +1,8 @@
+let d1 = Date.parse('2021-10-06 13:30:10')
+let d2 = Date.parse('2021-09-10 13:30:11')
+let dateFrom = Date.parse('2021-09-10')
+
+console.log(d1)
+console.log(d2)
+console.log(dateFrom)
+console.log(d1 >= dateFrom)
